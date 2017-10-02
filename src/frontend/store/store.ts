60c9f2a0531ec5503e9ded969dd807735901e0cc
Joe Seiden <joe.seiden214@gmail.com>
2017-10-02 11:10:48 -0700

    Reconcile changes

--- src/frontend/store/store.ts
+++ src/frontend/store/store.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import RootReducer from '../reducers/root_reducer'
 import StoreState from '../types/store_types'
